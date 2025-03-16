d91ae006fb6c97863a893a99df7416ff33e117e6
JOSUEALVAREZ22 <ar0978042024@unab.edu.sv>
2025-03-15 20:10:48 -0600

    Add files via upload
    Semana 5

---
+++ EJERCICIO SEMANA 5/Program.vb
@@ -0,0 +1,22 @@
+Imports System
+Imports ejercicio-semana5
+'se crea una llamada a la clase import'
+'se va a crear un objeto a partir de esta clase'
+'se apuntara a una funcion de la clase'
+Module Program
+    Sub main(args As String())
+        'se declara una variable'
+        'definimos el tipo de As tipo objeto'
+        'antes del nombre de la clase se debe usar new'
+        Dim bcfor As New ejemplo_bucles()
+        'static -> que se puede llamar sin instanciar la clase'
+        bcfor.buclefor(10)
+
+
+        'matodi main'
+        'crear nuestro objeto'
+        'dentro de est clase buscara el metodo'
+        'ejecutara la funcion'
+
+    End Sub
+End Module
---
+++ EJERCICIO SEMANA 5/ejemplo bucles.vb
@@ -0,0 +1,14 @@
+﻿Public Class ejemplo_bucles
+
+    Public Function buclefor(ByVal limite As Integer)
+        Dim i As Integer
+
+        For i = 0 To limite
+            Console.WriteLine(i)
+        Next
+
+    End Function
+
+
+
+End Class
